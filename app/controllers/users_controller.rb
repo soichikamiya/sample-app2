66e812379c26cf83301e2eb7da2753146760d139
EC2 Default User <ec2-user@ip-172-31-39-239.ap-northeast-1.compute.internal>
2019-08-02 14:19:01 +0000

    add flash[:success] & rspec valid_create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,9 @@ class UsersController < ApplicationController
     # @user = User.new(params[:user])
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      # redirect_to user_url(@user) と同じ
+      redirect_to @user
     else
       render 'new'
     end
--- spec/system/users_signup_spec.rb
+++ spec/system/users_signup_spec.rb
@@ -1,17 +1,30 @@
 require 'rails_helper'
 
-RSpec.describe '不正なサインアップ', type: :system do
-  it 'nameが空だと登録されない', js: true do
-    visit signup_path
-    expect do
-      post users_url, params: { user: { name: "", email: "useruser@yahoo.co.jp", 
-                                        password: "foobar", password_confirmation: "foobar" } }
-    end.not_to change(User, :count)
-    # end.to change(User, :count).by(1)
-    expect(response).to have_http_status(:success)
-    expect(response).to render_template :new
-    # expect(response).to redirect_to User.last
-    # expect(page).to have_selector 'div#error_explanation'
-    # expect(page).to have_selector 'div.alert-danger'
+RSpec.describe 'ユーザー登録', type: :system do
+  context '不正なサインアップ' do
+    it 'nameが空だと登録されない' do
+      visit signup_path
+      expect do
+        post users_url, params: { user: { name: "", email: "useruser@yahoo.co.jp",
+                                          password: "foobar", password_confirmation: "foobar" } }
+      end.not_to change(User, :count)
+      expect(response).to have_http_status(:success)
+      expect(response).to render_template :new
+      # expect(page).to have_selector 'div#error_explanation'
+      # expect(page).to have_selector 'div.alert-danger'
+    end
+  end
+
+  context '正常なサインアップ' do
+    it "正しい値だと登録される" do
+      visit signup_path
+      expect do
+        post users_url, params: { user: { name: "testuser", email: "useruser@yahoo.co.jp",
+                                          password: "foobar", password_confirmation: "foobar" } }
+      end.to change(User, :count).by(1)
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to User.last
+      # expect(page).to have_selector 'div.alert-success'
+    end
   end
 end
